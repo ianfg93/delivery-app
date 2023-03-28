a77808909d512125f1d97c596484a344c4bf84f3
igopereira1 <igopereira1@gmail.com>
2023-03-28 19:26:33 -0300

    feat: criando products model/migrations/seeders

---
+++ back-end/src/database/models/Products.model.js
@@ -0,0 +1,16 @@
+const ProductsModel = (sequelize, DataTypes) => {
+    const Product = sequelize.define('Product', {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      name: DataTypes.STRING,
+      price: DataTypes.DECIMAL(4, 2),
+      urlImage: DataTypes.STRING,
+    }, {
+      timestamps: false,
+      tableName: 'products',
+      underscored: true,
+    });
+  
+    return Product;
+  };
+  
+  module.exports = ProductsModel;
