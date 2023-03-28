25c1e12eb07610e50eab4b2262dab227fe3f609a
MarcosKern <vkg.marcos@gmail.com>
2023-03-28 15:06:23 -0300

    req 01

---
+++ front-end/src/context/Provider.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import MyContext from './Context';
+
+function Provider({ children }) {
+
+  return (
+    <MyContext.Provider value={ value }>
+      { children }
+    </MyContext.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import Provider from './context/Provider'
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
