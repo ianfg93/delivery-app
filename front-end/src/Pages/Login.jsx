3effe8bc5e377efa656e380eed1e3342cf1ba28e
MarcosKern <vkg.marcos@gmail.com>
2023-03-29 17:06:44 -0300

    req 03

--- front-end/src/Pages/Login.jsx
+++ front-end/src/Pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const COMMON = 'common_login';
 const MAIL = 'input-email';
@@ -6,8 +6,15 @@ const PASS = 'input-password';
 const LOGIN = 'button-login';
 const REGISTER = 'button-register';
 const INVALID = 'element-invalid-email';
+const PASS_MIN_LENGTH = 6;
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const emailRegex = () => /^\S+@\S+\.\S+$/i.test(email);
+  const passRegex = () => password.length >= PASS_MIN_LENGTH;
+
   return (
     <div>
       <div>Logo</div>
@@ -15,13 +22,29 @@ export default function Login() {
       <form action="post">
         <label htmlFor="inputMail">
           Login:
-          <input type="email" data-testid={ `${COMMON}__${MAIL}` } id="inputMail" />
+          <input
+            type="email"
+            data-testid={ `${COMMON}__${MAIL}` }
+            id="inputMail"
+            onInput={ ({ target: { value } }) => setEmail(value) }
+            value={ email }
+          />
         </label>
         <label htmlFor="inputPass">
           Senha:
-          <input type="password" data-testid={ `${COMMON}__${PASS}` } id="inputPass" />
+          <input
+            type="password"
+            data-testid={ `${COMMON}__${PASS}` }
+            id="inputPass"
+            onInput={ ({ target: { value } }) => setPassword(value) }
+            value={ password }
+          />
         </label>
-        <button type="submit" data-testid={ `${COMMON}__${LOGIN}` }>
+        <button
+          type="submit"
+          data-testid={ `${COMMON}__${LOGIN}` }
+          disabled={ !(emailRegex() && passRegex()) }
+        >
           Login
         </button>
         <button type="button" data-testid={ `${COMMON}__${REGISTER}` }>
