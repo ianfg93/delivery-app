612568487a8e701a14ab5f5deb55a372695b989c
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-04-03 19:37:13 -0300

    feat (req 18): tela customer checkout

--- front-end/src/Pages/Checkout.jsx
+++ front-end/src/Pages/Checkout.jsx
@@ -2,6 +2,18 @@ import React, { useContext } from 'react';
 import Navbar from '../Components/Navbar';
 import MyContext from '../context/Context';
 
+const ITEM = 'customer_checkout__element-order-table-item-number-';
+const DESCRIPTION = 'customer_checkout__element-order-table-name-';
+const QUANTITY = 'customer_checkout__element-order-table-quantity-';
+const PRICE = 'customer_checkout__element-order-table-unit-price-';
+const SUBTOTAL = 'customer_checkout__element-order-table-sub-total-';
+const REMOVE = 'customer_checkout__element-order-table-remove-';
+const TOTALPRICE = 'customer_checkout__element-order-total-price';
+const SELLER = 'customer_checkout__select-seller';
+const ADDRESS = 'customer_checkout__input-address';
+const NUMBER = 'customer_checkout__input-address-number';
+const SUBMIT = 'customer_checkout__button-submit-order';
+
 function Checkout() {
   const { cartTotal, cartQuantities } = useContext(MyContext);
 
@@ -23,29 +35,32 @@ function Checkout() {
         <tbody>
           {cartQuantities.map((product, index) => (
             <tr key={ product.productId }>
-              <td data-testid={ `customer_checkout__element-order-table-item-number-${index}` }>{product.id}</td>
-              <td data-testid={ `customer_checkout__element-order-table-name-${index}` }>{product.name}</td>
-              <td data-testid={ `customer_checkout__element-order-table-quantity-${index}`}>{product.quantity}</td>
-              <td data-testid={ `customer_checkout__element-order-table-unit-price-${index}`}>{product.price}</td>
-              <td data-testid={ `customer_checkout__element-order-table-sub-total-${index}`}>{(product.price * product.quantity).toFixed(2)}</td>
-              <td data-testid={ `customer_checkout__element-order-table-remove-${index}`}><button>Remover</button></td>
+              <td data-testid={ `${ITEM}${index}` }>{index + 1}</td>
+              <td data-testid={ `${DESCRIPTION}${index}` }>{product.name}</td>
+              <td data-testid={ `${QUANTITY}${index}` }>{product.quantity}</td>
+              <td data-testid={ `${PRICE}${index}` }>
+                {product.price.replace('.', ',')}
+              </td>
+              <td data-testid={ `${SUBTOTAL}${index}` }>
+                {(product.price * product.quantity).toFixed(2).replace('.', ',')}
+              </td>
+              <td data-testid={ `${REMOVE}${index}` }>
+                <button type="button">Remover</button>
+              </td>
             </tr>
           ))}
         </tbody>
-        {console.log(cartQuantities)}
       </table>
-      <p data-testid={ `customer_checkout__element-order-total-price` }>
-        Total:
-        R$
+      <p data-testid={ TOTALPRICE }>
         {cartTotal}
       </p>
       <div>
-        <select data-testid="customer_checkout__select-seller" ><option value="Ze">Zé</option></select>
-        <input type="text" data-testid="customer_checkout__input-address" />
+        <select data-testid={ SELLER }><option value="Ze">Zé</option></select>
+        <input type="text" data-testid={ ADDRESS } />
         Endereço
-        <input type="number" data-testid="customer_checkout__input-address-number" />
+        <input type="number" data-testid={ NUMBER } />
         Número
-        <button data-testid="customer_checkout__button-submit-order">FINALIZAR PEDIDO</button>
+        <button type="button" data-testid={ SUBMIT }>FINALIZAR PEDIDO</button>
       </div>
     </div>
   );
