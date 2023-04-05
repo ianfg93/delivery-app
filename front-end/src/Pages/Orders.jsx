8883b58f6073dfe0c3b5ba52c7e33ed679fec6e5
MarcosKern <vkg.marcos@gmail.com>
2023-04-05 02:08:52 -0300

    reqs 24 ok

--- front-end/src/Pages/Orders.jsx
+++ front-end/src/Pages/Orders.jsx
@@ -3,36 +3,42 @@ import Navbar from '../Components/Navbar';
 import MyContext from '../context/Context';
 
 export default function Orders() {
-  const { orders, getOrders } = useContext(MyContext);
+  const { orders, getOrders, navigate } = useContext(MyContext);
+
   useEffect(() => {
     getOrders(JSON.parse(localStorage.getItem('user')).id);
   }, []);
+
   return (
     <div>
       <Navbar />
       <div>
         { orders.length !== 0 && orders.map((order) => (
-          <button type="button" key={ order.id }>
+          <button
+            type="button"
+            key={ order.id }
+            onClick={ () => navigate(`/customer/orders/${order.id}`) }
+          >
             <div
-              data-testid={ `customer_orders_element-order-id-${order.id}` }
+              data-testid={ `customer_orders__element-order-id-${order.id}` }
             >
               <p>pedido</p>
               { order.id }
             </div>
             <div
-              data-testid={ `customer_orders_element-delivery-status-${order.id}` }
+              data-testid={ `customer_orders__element-delivery-status-${order.id}` }
               className={ order.status }
             >
               { order.status }
             </div>
             <div>
               <p
-                data-testid={ `customer_orders_element-order-date-${order.id}` }
+                data-testid={ `customer_orders__element-order-date-${order.id}` }
               >
                 { order.saleDate }
               </p>
               <p
-                data-testid={ `customer_orders_element-card-price-id-${order.id}` }
+                data-testid={ `customer_orders__element-card-price-${order.id}` }
               >
                 R$:
                 {' '}
