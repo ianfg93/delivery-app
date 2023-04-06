aa10eb8a09e9f97fdb974b274c672110d6e3fd32
igopereira1 <igopereira1@gmail.com>
2023-04-06 20:48:40 -0300

    new fix (req 25 e 26)

--- front-end/src/Pages/OrderDetails.jsx
+++ front-end/src/Pages/OrderDetails.jsx
@@ -14,23 +14,20 @@ export default function OrderDetails() {
   useEffect(() => {
     async function awaitData() {
       const response = await getOrderDetails(id);
-      console.log(response);
       setDetails(response);
       setShowDetails(true);
     }
     awaitData();
-    console.log(details);
   }, []);
 
   const convertDate = (obj) => {
+    const LIMIT = 9;
     const date = obj.saleDate;
     const year = new Date(date).getFullYear();
     const month = new Date(date).getMonth() + Number(1);
-    console.log(month);
-    console.log(new Date(date));
     const day = new Date(date).getDate();
-    const finalMonth = +month > 9 ? month.toString() : `0${month}`;
-    const finalDay = +day > 9 ? day.toString() : `0${day}`;
+    const finalMonth = +month > LIMIT ? month.toString() : `0${month}`;
+    const finalDay = +day > LIMIT ? day.toString() : `0${day}`;
     const finalDate = `${finalDay}/${finalMonth}/${year}`;
 
     return finalDate;
